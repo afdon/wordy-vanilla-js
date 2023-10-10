2cb1e7f32e913d005c460ff60540967260409f2f
afdon <53676753+afdon@users.noreply.github.com>
2023-10-09 17:08:52 -0700

    cleanup

--- script.js
+++ script.js
@@ -9,21 +9,15 @@ async function getAnswer() {
   try {
     const response = await fetch("https://words.dev-apis.com/word-of-the-day")
     const body = await response.json();
-    console.log("The word is", body.word) 
-    // why is there automatically a space before the word?
+    // console.log("The word is", body.word) 
     return body.word;
   } catch (error) {
     console.error(`There was an error: ${error}.`)
   }
 };
 
-// getAnswer().then(response => answer = response)
-// console.log("answer:", answer)
-// or
-
 async function main() {
   answer = await getAnswer();
-  // console.log("answer:", answer);
 }
 main()
 
@@ -40,20 +34,6 @@ async function validateWord(data = {}) {
   }
 }
 
-// if (unvalidatedGuess.length === lettersPerWord) {
-
-//   validateWord({ word: unvalidatedGuess }).then((data) => {
-//     // JSON data parsed by `data.json()` call:
-//     console.log(`The word ${data.word} is valid: ${data.validWord}`); 
-//     if (data.validWord) {
-//       validGuess = data.word
-//     } else {
-//       alert(`Sorry, "${data.word}" isn't a valid word.`)
-//     }
-//   })
-
-// }
-
 let guesses = [];
 
 function isLetters(string) {
@@ -68,7 +48,6 @@ function isLetters(string) {
      return false;
      }
   }
-  // console.log(isLetters("hello"))
 
 const isFiveChars = (string) => {
   if (string.length === 5) return true;
@@ -77,7 +56,7 @@ const isFiveChars = (string) => {
 let box = document.querySelector("input");
 
 box.addEventListener("keyup", function (e) {
-  console.log(e.key); // this.value?
+  console.log(e.key); 
 
   if (e.key.length === 1 && isLetters(e.key)) unvalidatedGuess = unvalidatedGuess + (e.key);
 
@@ -88,40 +67,21 @@ box.addEventListener("keyup", function (e) {
 
   if (unvalidatedGuess.length === lettersPerWord) {
       validateWord({ word: unvalidatedGuess }).then((data) => {
-        // JSON data parsed by `data.json()` call:
         console.log(`The word ${data.word} is valid: ${data.validWord}.`);
         if (data.validWord) {
           unvalidatedGuess = ""
           clearInput()
           // run the checkGuess function here.
           let status = checkGuess(data.word).charsStatus
-          console.log(`The first char is in the correct position: ${status[0].isCorrectPosition}. Is included: ${status[0].isIncluded}.`);
+          // console.log(`The first char is in the correct position: ${status[0].isCorrectPosition}. Is included: ${status[0].isIncluded}.`);
           guesses.push(data.word)
-
-          // now display in UI
-          // const firstRow = document.querySelectorAll(`.row-1 > div`)
-          // firstRow.forEach((element, i) => element.innerHTML = guesses[0][i])
-          // // why is the console log not defined?
-          // // console.log(`first row: guesses[0] is ${guesses[0][i]}`)
-          // firstRow.forEach((e, i) => {
-          //   let statusOfGuess1 = checkGuess(guesses[0]).charsStatus
-          //   if (statusOfGuess1[i].isCorrectPosition) {
-          //     e.style.backgroundColor = "green";
-          //   } else if (statusOfGuess1[i].isIncluded) {
-          //     e.style.backgroundColor = "yellow"
-          //   } else {
-          //     e.style.backgroundColor = "grey"
-          //   }
-          // })
 
           rows = document.querySelectorAll(`.board > .row`)
           rows.forEach((row, i) => {
             if (!guesses[i]) return
-            console.log("guesses[i]2", i, guesses[i], guesses, rows.length)
+            // console.log("guesses[i]2", i, guesses[i], guesses, rows.length)
             let statusOfGuess = checkGuess(guesses[i]).charsStatus
-            console.log("guesses[i]", guesses[i])
-            // why did the below not work?
-            // let divs = document.querySelectorAll(`.row > div`)
+            // console.log("guesses[i]", guesses[i])
             let cells = row.querySelectorAll(`div`)
             cells.forEach((cell, j) => {
               if (guesses[i][j]) {cell.innerHTML = guesses[i][j]}
@@ -134,25 +94,12 @@ box.addEventListener("keyup", function (e) {
               }
             })
           })
-
-          // const secondRow = document.querySelectorAll(`.row-2 > div`)
-          // secondRow.forEach((element, i) => element.innerHTML = guesses[1][i])
-          // const thirdRow = document.querySelectorAll(`.row-3 > div`)
-          // thirdRow.forEach((element, i) => element.innerHTML = guesses[2][i])
-          // const fourthRow = document.querySelectorAll(`.row-4 > div`)
-          // fourthRow.forEach((element, i) => element.innerHTML = guesses[3][i])
-          // const fifthRow = document.querySelectorAll(`.row-5 > div`)
-          // fifthRow.forEach((element, i) => element.innerHTML = guesses[4][i])
-          // const sixthRow = document.querySelectorAll(`.row-6 > div`)
-          // sixthRow.forEach((element, i) => element.innerHTML = guesses[5][i])
-
-          
           
           console.log(guesses)
           const didWin = checkWin()
           if (didWin) {
             console.log(`You got it! The answer is ${answer}.`)
-            // location.reload() // don't reload, at least not right away
+            // location.reload() // don't reload
           } else if (guesses.length === maxGuesses) {
               console.log(`Sorry, you lose. The answer was ${answer}. Try again tomorrow.`)
             
@@ -215,24 +162,15 @@ function checkGuess(guess) {
   };
 }
 
-// guesses.push(checkGuess("hello"));
-// guesses.push(checkGuess("whatz"));
-// guesses.push(checkGuess("wordz"));
-// guesses.push(checkGuess("words"));
-
 console.log("guesses: ", guesses);
 
-// function handleGuess(guess) {
-//   let status = checkGuess(guess);
+// function displayGuesses() {
+//   for (let i = 0; i < guesses.length; i++) {
+//     // console.log("guesses[i]2", guesses[i])
+//     if (guesses[i]) {
+//       let charStatuses = checkGuess(guesses[i])
+//       // console.log("charStatuses", charStatuses)
+//     }
+//   }
 // }
-
-function displayGuesses() {
-  for (let i = 0; i < guesses.length; i++) {
-    console.log("guesses[i]2", guesses[i])
-    if (guesses[i]) {
-      let charStatuses = checkGuess(guesses[i])
-      console.log("charStatuses", charStatuses)
-    }
-  }
-}
 
